9d2f3a4b7d237c6328aa6eed3dcdc37184a1dba1
Matt Miller and semhar Kidane <matt@>
2018-05-22 16:07:45 -0700

    Add functionality to pass first test.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,7 +1,25 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
     public String findAndReplce(String sentence, String wordToReplace, String replacementWord) {
-        return null;
+
+        if(sentence.equals(wordToReplace)){
+            return replacementWord;
+        } else {
+            return sentence;
+        }
+
+
+//        List<String> words = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
+//        for (int i = 0; i < words.size() ; i++) {
+//            if(words.get(i).equals(wordToReplace)) {
+//                words.set(i, replacementWord);
+//            }
+//        }
+//        return ;
     }
 }
