db46c5bc62577a789f5d75487377e0fddaba0959
Matt Miller and semhar Kidane <semhar@>
2018-05-22 16:22:20 -0700

    Add logic to pass second test.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -7,19 +7,20 @@ import java.util.List;
 public class Game {
     public String findAndReplce(String sentence, String wordToReplace, String replacementWord) {
 
-        if(sentence.equals(wordToReplace)){
-            return replacementWord;
-        } else {
-            return sentence;
-        }
-
-
-//        List<String> words = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
-//        for (int i = 0; i < words.size() ; i++) {
-//            if(words.get(i).equals(wordToReplace)) {
-//                words.set(i, replacementWord);
-//            }
+//        if(sentence.equals(wordToReplace)){
+//            return replacementWord;
+//        } else {
+//            return sentence;
 //        }
-//        return ;
+
+
+        List<String> words = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
+        for (int i = 0; i < words.size() ; i++) {
+            if(words.get(i).equals(wordToReplace)) {
+                words.set(i, replacementWord);
+            }
+        }
+        String result = String.join(" ", words);
+        return result;
     }
 }
