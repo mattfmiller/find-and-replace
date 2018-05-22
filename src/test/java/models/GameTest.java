8b3908206200352b1344c4ee71d0af2ada55ace6
Matt Miller and semhar Kidane <semhar@>
2018-05-22 16:12:11 -0700

    Add and fail second test for multi word sentences.

--- src/test/java/models/GameTest.java
+++ src/test/java/models/GameTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.*;
 
 public class GameTest {
     @Test
-    public void findAndReplace_replaceASingleWord_y() throws Exception{
+    public void findAndReplace_replaceWordInSingleWordSentence_y() throws Exception{
         Game testGame = new Game();
         String expected = "y";
         assertEquals(expected, testGame.findAndReplce("x", "x", "y"));
+    }
+
+    @Test
+    public void findAndReplace_replaceWordInMultiWordSentence_yghu() throws Exception{
+        Game testGame = new Game();
+        String expected = "y g h u";
+        assertEquals(expected, testGame.findAndReplce("x g h u", "x", "y"));
     }
 }
